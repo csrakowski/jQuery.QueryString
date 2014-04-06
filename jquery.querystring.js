863b02257fdc05f00b8144fc32c94a60c0ec8d79
Christiaan Rakowski <cs.rakowski@gmail.com>
2014-04-06 14:54:03 +0200

    initial commit

---
+++ jquery.querystring.js
@@ -0,0 +1,27 @@
+(function ($) {
+    "use strict";
+
+    var _queryString = (function () {
+        var result = new Array();
+        try  {
+            var args = location.search.substr(1).split('&');
+            var len = args.length;
+            for (var i = 0; i < len; i++) {
+                var param = args[i].split('=');
+                result[param[0]] = decodeURIComponent(param[1]);
+            }
+        } catch (e) {
+        }
+        return result;
+    })();
+
+    $.Querystring = function (key) {
+        if (key === "#") {
+            if (location.hash) {
+                return location.hash.substr(1);
+            }
+        } else {
+            return _queryString[key];
+        }
+    };
+})(jQuery);
---
+++ jquery.querystring.ts
@@ -0,0 +1,32 @@
+interface JQuery {
+	Querystring: (key:string) => string;
+}
+declare var jQuery: JQuery;
+
+(function ($: JQuery) {
+	"use strict";
+
+	var _queryString = (function () {
+		var result = new Array<String>();
+		try {
+			var args = location.search.substr(1).split('&');
+			var len = args.length;
+			for (var i = 0; i < len; i++) {
+				var param = args[i].split('=');
+				result[param[0]] = decodeURIComponent(param[1]);
+			}
+		} catch (e) {
+		}
+		return result;
+	})();
+
+	$.Querystring = function (key: string) {
+		if (key === "#") {
+			if (location.hash) {
+				return location.hash.substr(1);
+			}
+		} else {
+			return _queryString[key];
+		}
+	};
+})(jQuery);
