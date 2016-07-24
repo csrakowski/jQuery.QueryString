4c9a4913e5a4824daa12da61d9463aa1d0930eea
Christiaan Rakowski <cs.rakowski@gmail.com>
2016-07-24 11:36:16 +0200

    Refactored all the things!

--- output/jquery.querystring.js
+++ output/jquery.querystring.js
@@ -1,35 +1,36 @@
-/// <reference path="jquery.querystring.d.ts" />
-
+//#endregion
 (function ($) {
     "use strict";
-
+    var decode = decodeURIComponent;
     $.QueryStringParser = function (url) {
         var result = new Array();
-        try  {
+        try {
             var parts = (url.split("?")[1]).split("#");
             url = parts[0];
             if (parts[1]) {
-                result["#"] = decodeURIComponent(parts[1]);
+                result["#"] = decode(parts[1]);
             }
-            var args = url.split('&');
+            var args = url.split("&");
             var len = args.length;
             for (var i = 0; i < len; i++) {
-                var param = args[i].split('=');
-                result[param[0]] = decodeURIComponent(param[1]);
+                var param = args[i].split("=");
+                result[param[0]] = decode(param[1]);
             }
-        } catch (e) {
+        }
+        catch (e) {
         }
         return result;
     };
-
     var _queryString = $.QueryStringParser(location.search);
     $.Querystring = function (key) {
         if (key === "#") {
             if (location.hash) {
                 return location.hash.substr(1);
             }
-        } else {
+        }
+        else {
             return _queryString[key];
         }
     };
 })(jQuery);
+//# sourceMappingURL=jquery.querystring.js.map
---
+++ src/jquery.querystring.ts
@@ -0,0 +1,46 @@
+//#region JQueryStatic
+//Added here for simplicity
+interface JQueryStatic {
+	//Gets the querystring parameter for the key specified (returns undefined if no value is present)
+	Querystring(key: string): string;
+	QueryStringParser(url: string): string[];
+}
+declare var jQuery: JQueryStatic;
+//#endregion
+
+
+(function ($: JQueryStatic) {
+	"use strict";
+
+	let decode = decodeURIComponent;
+
+	$.QueryStringParser = function (url: string) {
+		var result = new Array<string>();
+		try {
+			let parts = (url.split("?")[1]).split("#");
+			url = parts[0];
+			if (parts[1]) {
+				result["#"] = decode(parts[1]);
+			}
+			var args = url.split("&");
+			var len = args.length;
+			for (let i=0; i<len; i++) {
+				let param = args[i].split("=");
+				result[param[0]] = decode(param[1]);
+			}
+		} catch (e) {
+		}
+		return result;
+	};
+
+	var _queryString = $.QueryStringParser(location.search);
+	$.Querystring = function (key: string) {
+		if (key === "#") {
+			if (location.hash) {
+				return location.hash.substr(1);
+			}
+		} else {
+			return _queryString[key];
+		}
+	};
+})(jQuery);
